d2e2a1060cbf103abcff0e6add3fe4dc0aa53a17
xiaoa <towanm@foxmail.com>
2019-03-07 17:35:13 +0800

    增加404的枚举

--- src/main/java/common/enums/ExceptionEnum.java
+++ src/main/java/common/enums/ExceptionEnum.java
@@ -9,6 +9,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public enum ExceptionEnum {
     PRICE_CANNOT_BE_NULL(400, "价格不能为空"),
+    PATH_IS_NOT_TOUCH(404,"路径无法访问"),
     ;
     private int code;
     private String msg;
